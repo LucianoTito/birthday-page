56a7c37eb07c074e5da5fc80234f9f8f91ff8672
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2023-03-17 02:29:27 -0300

    probando formulario

---
+++ messages.php
@@ -0,0 +1,17 @@
+<?php
+include_once('db.php');
+/*Aquí recibo todos los datos del formulario */
+$nombre= $_POST['nombre'];
+$mensaje= $_POST['dedicatoria'];
+
+echo "Los datos son los siguientes <br>";
+echo "$nombre, $mensaje";
+
+$conectar=conectar();
+$sql = "INSERT INTO dedicatoria (nombre, mensaje)
+VALUES ( '$nombre', '$mensaje')";
+$resul = mysqli_query($conectar, $sql) or trigger_error("Query Failed! SQL -Error: ".mysqli_error($conectar),
+E_USER_ERROR);
+
+echo "$sql";
+?>
